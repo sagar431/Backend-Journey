149b12bc71f355e6add1ab5bee80ae0ed65ab26f
sagar431 <sagarpallai1997@gmail.com>
2024-07-04 11:47:39 +0530

    Data types and EMCA

---
+++ Java scirpt/02_Datatype.js
@@ -0,0 +1,35 @@
+"use strict";// treat all js code as newer version
+
+
+// alert(3+3) // we are using nodejs ,not browser
+
+console.log(3+3);
+
+console.log("sagar") // code relabity  should be high 
+
+
+
+let name="sagar"
+
+let age=18
+
+let isLoggedIn=false
+
+// number => 2  to power 53 
+
+// bigint
+// string =>""
+
+// boolean => true/false
+
+//null=> standalone value
+
+// undefined =>
+
+// symbol => unique
+
+//object 
+
+
+
+console.log(typeof"sagar")
