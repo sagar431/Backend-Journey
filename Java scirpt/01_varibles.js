1977d0863d39cd3ada902244fd2c1d191547c2b8
sagar431 <sagarpallai1997@gmail.com>
2024-07-04 11:35:12 +0530

    Let,const and var implmneted

---
+++ Java scirpt/01_varibles.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="sagar89@gmail.com"
+var accounrpassword="12234"
+accountCity="Bbsr"
+
+//accountId=2
+ accountEmail="sagar@.com"
+ accounrpassword="42142144"
+ accountCity="Kendrpara"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accounrpassword,accountCity])
